50886718243a9578902e737e7d2964b2012ba74e
Simkin Andrew <s.a.s.s.e.r@mail.ru>
2019-12-15 18:18:24 +0300

    Add bootstrap

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,11 +5,23 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel test project</title>
+
+        <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+        <script type="text/javascript" src="{{ mix('/js/app.js') }}"></script>
+        
+        <script src="https://use.fontawesome.com/441fced82f.js"></script>
+
+
     </head>
     <body>
-        
+       
         <div class="container">
-            <h1>Hello, world!</h1>
+            <section id="main">
+                <h1><i class="fa fa-address-book" aria-hidden="true"></i> Test project Laravel</h1>
+                
+                <div class="alert alert-success" role="alert"><span class="glyphicon glyphicon-align-left" aria-hidden="true"></span> Bootstrap was installed</div>
+            </section>
         </div>
+        
     </body>
 </html>
